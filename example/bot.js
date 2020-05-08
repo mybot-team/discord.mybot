fe4022d952db84387c5a7bbdfaae6405b5ef1ebf
Drylotrans <35281350+Drylotrans@users.noreply.github.com>
2020-05-08 10:05:56 -0500

    Renamed files and created src/

--- example/bot.js
+++ example/bot.js
@@ -1,6 +1,5 @@
-const Discord = require('discord.mybot');
+const Discord = require('../');
 const client = new Discord.Client();
 
 // Token bot en: https://discordapp.com/developers/applications
-client.login('BOT_TOKEN')
-
+client.login('BOT_TOKEN');
